d5c09f029e5a6e6a9349a91bb7015beec2c92fbd
nickbobo <934858124@qq.com>
2016-12-19 14:36:15 +0800

    a

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            Hello World!!!112<br />
+            欢迎来到菜鸟教程学习！！！12121212
+            <div>wrwerwe</div>
+         </div>
+      );
+   }
+}
+
+export default App;
